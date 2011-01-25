75954eb06370d2041890cc1d7cca7e3b9dc534e7
stefanfoulis <stefan.foulis@gmail.com>
2011-01-25 14:14:52 +0100

    remove print statement

--- cmsplugin_banner/context_processors.py
+++ cmsplugin_banner/context_processors.py
@@ -1,6 +1,6 @@
 from cmsplugin_banner import conf
 from cmsplugin_banner import models
 def plugin_ad_slots(request):
+    # TODO: this should be cached
     slots = list(models.Slot.objects.filter(banners__gt=0))
-    print slots
     return {'CMSPLUGIN_BANNER_SLOTS': slots,}
